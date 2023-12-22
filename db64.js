184d36a69ec74f35034a7f15f5c39e894a93829d
Julien Etienne <j8@envidia.io>
2023-12-22 04:42:02 +0000

    Clean up

--- db64.js
+++ db64.js
@@ -1,16 +1,11 @@
 const { isArray } = Array
-const err = console.error
-const openDBErrMessage = e => err(`Error creating database: ${e}`)
-const setDataError = e => err('Error setting data', e)
-const getDataError = e => err('Error retrieving data', e)
-let lastDBName
-let lastStoreName
+
 const openDB = (name = 'default', storeNames) => new Promise((resolve, reject) => {
     let db
     try {
         db = window.indexedDB.open(name, 1)
     } catch (e) {
-        reject(openDBErrMessage)
+        reject(e)
     }
 
     db.onupgradeneeded = e => {
@@ -104,15 +99,12 @@ let hasDBandStores = false
 const db64 = {
     create: async (name, storeNames) => {
         if(!isArray(storeNames)) return console.error('storeNames should be an array')
-        lastDBName = name
-        lastStoreName = storeNames[0]
+
         await openDB(name, storeNames, hasDBandStores)
         return db64
     },
     use: (name, storeName) => {
         if(!hasDBandStores) return console.error('A database and store needs to be created first')
-        lastDBName = name
-        lastStoreName = storeName
 
         return {
             set: async (key, value) => openDB(name, storeName)
@@ -121,32 +113,24 @@ const db64 = {
             ,
             setEntries: async (value) => openDB(name, storeName)
                 .then(db => setData(db, storeName, null, value, 'entries'))
-                .catch(setDataError)
+                .catch(console.error)
             ,
             get: async key => openDB(name, storeName)
                 .then(db => getData(db, storeName, key))
-                .catch(getDataError)
+                .catch(console.error)
             ,
             getEntries: async (keys) => openDB(name, storeName)
                 .then(db => getData(db, storeName, keys, 'entries'))
-                .catch(getDataError)
+                .catch(console.error)
             ,
             delete: async (keys) => openDB(name, storeName)
                 .then(db => deleteData(db, storeName, keys))
                 .catch(console.error)
         }
-     
     },
     clear: async (name, storeName) => openDB(name, storeName)
         .then(db => clearStore(db, storeName))
         .catch(console.error)
 }
-
-// const getLastDBandStore = () => db64.use(lastDBName,lastStoreName)
-// db64.set = (key, value) => getLastDBandStore().set(key, value)
-// db64.setEntries = entries => getLastDBandStore().setEntries(entries)
-// db64.get = key => getLastDBandStore().get(key)
-// db64.getEntries = entries => getLastDBandStore().getEntries(entries)
-// db64.delete = keys => getLastDBandStore().delete(keys)
 
 export default db64
